b6fbdebf126d2218dcea8386b4ea43f2a44b15f1
Tobias Weber <tobias.weber@lrz.de>
2017-12-06 10:21:16 +0100

    Make node playbook idempotent

--- roles/common/files/printIface.sh
+++ roles/common/files/printIface.sh
@@ -3,10 +3,19 @@ curNum=0
 for iface in `ip a | grep -v ovs-system | grep 'state UP' | awk -F ':' '{ print $2 }'`
 do
   let "curNum++"
-  if [ $curNum == $1 ]
+  if [ "$curNum" -eq "$1" ]
   then
-    echo -n "$iface "
-    ip a | grep inet | grep $iface | awk '{ print $2 }' | awk -F '/' '{ print $1 }' | tr '\n' ' '
-    ip route | grep default | grep $iface | awk '{ print $3 }'
+    state=UP
+    ip link show up | grep -q $iface || state=DOWN
+    ip=$(ip a | grep inet | grep $iface | awk '{ print $2 }' | awk -F '/' '{ print $1 }')
+    gw=$(ip route | grep default | grep $iface | awk '{ print $3 }')
+    nw=$(ipcalc $(ip a | grep inet | grep $iface | awk '{ print $2 }' | awk -F '/' '{ print $1 }' | tr '\n' ' ') | grep 'Network:' | awk '{ print $2 }')
+    min=$(ipcalc $(ip a | grep inet | grep $iface | awk '{ print $2 }' | awk -F '/' '{ print $1 }' | tr '\n' ' ') | grep 'HostMin:' | awk '{ print $2 }')
+    max=$(ipcalc $(ip a | grep inet | grep $iface | awk '{ print $2 }' | awk -F '/' '{ print $1 }' | tr '\n' ' ') | grep 'HostMax:' | awk '{ print $2 }')
+    output="$iface $state $ip $gw $nw $min $max"
+    echo $output | tr '\n' ' '
+    echo
+    exit 0
   fi
 done
+exit 100
