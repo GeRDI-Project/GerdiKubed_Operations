05fb70d75c02fd5aba0cfec9ab8d961a9fe2c011
Tobias Weber <tobias.weber@lrz.de>
2017-12-06 09:22:14 +0100

    Parametrized iface repair scripts

--- util/repairIface.sh
+++ util/repairIface.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 ip=__IP__
-set -f
-IFS='
-'
+gw=__GW__
+nw=__NW__
+if=__IF__
+table="rt2"
 
+# Delete entries from systemd
+ip route | grep $gw | grep -q default \
+  && ip route delete default via $gw dev $if \
+  || echo "deleted default route in standard table"
+ip route | grep $nw \
+  && ip route delete $nw dev $if \
+  || echo "deleted route 1 in standard table"
 
-ip route delete default via 141.40.255.254 dev ens3
-ip route delete 141.40.254.0/23 dev ens3
+# Make sure table $table exists
+grep -q $table /etc/iproute2/rt_tables \
+  || echo "1 $table" >> /etc/iproute2/rt_tables \
+  || echo "added table $table to rt_tables"
 
-echo
-grep -q rt2 /etc/iproute2/rt_tables || echo "1 rt2" >> /etc/iproute2/rt_tables
+# Add routes and rules to table $table
+ip route show table $table | grep -q $nw \
+  || ip route add $nw dev $if src $ip table $table
 
-ip route add 141.40.254.0/23 dev ens3 src $ip table rt2
-ip route add default via 141.40.255.254 dev ens3 src $ip table rt2
-ip rule add from 141.40.254.0/23 table rt2
-ip rule add to 141.40.254.0/23 table rt2
+ip route show table $table | grep -q default \
+  || ip route add default via $gw dev $if src $ip table $table
+
+ip rule show table $table | grep from | grep -q $nw \
+  || ip rule add from $nw table $table
+
+ip rule show table $table | grep to | grep -q $nw \
+  || ip rule add to $nw table $table
+
