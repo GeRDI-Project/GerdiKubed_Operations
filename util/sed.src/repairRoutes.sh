918af6975747233fa77c04782c8615638ef7f9f5
Tobias Weber <tobias.weber@lrz.de>
2018-01-15 15:45:31 +0100

    [SAI-389] Documentation added

--- util/sed.src/repairRoutes.sh
+++ util/sed.src/repairRoutes.sh
@@ -7,5 +7,7 @@ do
 done
 
 # Add routingpolicies
+# Beginnin with systemd 236 we can do it by a configuration.
+# Alas, debian 9 has systemd 232, so we need to do it manually.
 ip rule add from __IP_INT__ lookup __ROUTING_TABLE_INT__
 ip rule add to  __NW_INT__ lookup __ROUTING_TABLE_INT__
